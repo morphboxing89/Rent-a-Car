3b9abf2595671ce591d719d119074d79813082e5
morphboxing89 <phenomen0m@mail.ru>
2024-06-28 13:29:58 +0300

    Rent-a-Car

---
+++ part_1/block_1/Rent_a_Car.sql
@@ -0,0 +1,33 @@
+CREATE TABLE Сustumers --Клиенты
+(
+    id SERIAL PRIMARY KEY,
+    firstName TEXT NOT NULL,
+    middlename TEXT,
+    lastName TEXT NOT NULL,
+    Phone VARCHAR(11) NOT NULL UNIQUE
+);
+
+
+CREATE TABLE Autos --Автомобили
+  (  id INT PRIMARY KEY,
+    brand TEXT NOT NULL,
+    rental_price FLOAT,
+    type_rental TEXT NOT NULL
+);
+
+
+CREATE TABLE Issued_cars --Выданные автомобили
+(
+    id INT PRIMARY KEY,
+    custumer_id INT,
+    autos_id INT,
+    total_price FLOAT,
+    created_date CURRENT_DATE,
+
+    FOREIGN KEY (custumer_id) REFERENCES Custumers(id),
+    FOREIGN KEY (autos_id) REFERENCES Autos(id)
+);
+
+-- INSERT INTO Custuners (firstName,middleName, lastName)
+--     VALUES('Artem', 'Alexsandrovich', 'Sirotin'),
+--            ('Alex', 'Alexsandrovich', 'Batashev');
