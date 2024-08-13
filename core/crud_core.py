a51f48c632d0f80d660e1cf003c1d4e6d46ceb72
morphboxing89 <phenomen0m@mail.ru>
2024-08-13 13:25:34 +0300

    Update crud_core.py

--- core/crud_core.py
+++ core/crud_core.py
@@ -35,16 +35,15 @@ IssuedCars = crud_core.Table(
 meta.drop_all(engine)
 meta.create_all(engine)
 
-
-
 # ==============================Заполнение================================
 with engine.connect() as conn:
     input_1 = crud_core.insert(Customers).values(first_name='Artem',
-                                            middle_name='Aleksandovich',
-                                            last_name='Sirotin',
-                                            phone='79998207171')
-    input_2 = crud_core.insert(Customers).values(first_name='Alex', middle_name='Aleksandovich', last_name='Batashev', phone='79998207272')
-    input_3 = crud_core.insert(Customers).values(first_name='Oleg', middle_name='Olegovich', last_name='Olegov', phone='79998207475')
+                                                 middle_name='Aleksandovich',
+                                                 last_name='Sirotin', phone='79998207171')
+    input_2 = crud_core.insert(Customers).values(first_name='Alex', middle_name='Aleksandovich', last_name='Batashev',
+                                                 phone='79998207272')
+    input_3 = crud_core.insert(Customers).values(first_name='Oleg', middle_name='Olegovich', last_name='Olegov',
+                                                 phone='79998207475')
     conn.execute(input_1)
     conn.execute(input_2)
     conn.execute(input_3)
@@ -54,14 +53,13 @@ with engine.connect() as conn:
     conn.commit()
 
 
-
 with engine.connect() as conn:
-    input_1 = crud_core.insert(Autos).values(brand='Audi Q5', rental_price = '7000', type_rental='one day')
-    input_2 = crud_core.insert(Autos).values(brand='Audi Q5', rental_price ='9000', type_rental='one day')
-    input_3 = crud_core.insert(Autos).values(brand='Audi Q5', rental_price ='9000', type_rental='one day')
-    input_4 = crud_core.insert(Autos).values(brand='Mazda 6', rental_price ='8000', type_rental='one day')
-    input_5 = crud_core.insert(Autos).values(brand='Mazda 6', rental_price ='8000', type_rental='one day')
-    input_6 = crud_core.insert(Autos).values(brand='Mazda 6', rental_price ='6000', type_rental='one day')
+    input_1 = crud_core.insert(Autos).values(brand='Audi Q5', rental_price='7000', type_rental='one day')
+    input_2 = crud_core.insert(Autos).values(brand='Audi Q5', rental_price='9000', type_rental='one day')
+    input_3 = crud_core.insert(Autos).values(brand='Audi Q5', rental_price='9000', type_rental='one day')
+    input_4 = crud_core.insert(Autos).values(brand='Mazda 6', rental_price='8000', type_rental='one day')
+    input_5 = crud_core.insert(Autos).values(brand='Mazda 6', rental_price='8000', type_rental='one day')
+    input_6 = crud_core.insert(Autos).values(brand='Mazda 6', rental_price='6000', type_rental='one day')
     conn.execute(input_1)
     conn.execute(input_2)
     conn.execute(input_3)
@@ -76,17 +74,17 @@ with engine.connect() as conn:
 
 with engine.connect() as conn:
     input_1 = crud_core.insert(IssuedCars).values(
-                                        customer_id = 1,
-                                        autos_id = 1,
-                                        total_price = 7000)
+                                        customer_id=1,
+                                        autos_id=1,
+                                        total_price=7000)
     input_2 = crud_core.insert(IssuedCars).values(
-                                        customer_id = 2,
-                                        autos_id = 2,
-                                        total_price = 9000)
+                                        customer_id=2,
+                                        autos_id=2,
+                                        total_price=9000)
     input_3 = crud_core.insert(IssuedCars).values(
-                                        customer_id = 3,
-                                        autos_id = 3,
-                                        total_price = 8000)
+                                        customer_id=3,
+                                        autos_id=3,
+                                        total_price=8000)
     conn.execute(input_1)
     conn.execute(input_2)
     conn.execute(input_3)
@@ -97,23 +95,20 @@ with engine.connect() as conn:
 with engine.connect() as conn:
 
     # -
-    # update_1 = crud_core.update(Customers).where(Customers.c.id == 1).values(first_name = 'Timur')
+    update_1 = crud_core.update(Customers).where(Customers.c.id == 1).values(first_name='Timur')
 
-    # select_test1 = crud_core.select(Customers)
-    # print(conn.execute(select_test1).all())
-    
-    
-    # update_2 = crud_core.update(Autos).where(Autos.c.rental_price).values(rental_price = 12000)
+    select_test1 = crud_core.select(Customers)
+    print(conn.execute(select_test1).all())
 
-    # select_test1 = crud_core.select(Autos)
-    # print(conn.execute(select_test1).all())
-    
-    
-    # update_3 = crud_core.update(Autos).where(Autos.c.brand == 'Mazda 6').values(brand = 'BMW X5')
-
+    # update_2 = crud_core.update(Autos).where(Autos.c.rental_price).values(rental_price=12000)
+    #
     # select_test1 = crud_core.select(Autos)
     # print(conn.execute(select_test1).all())
 
+    # update_3 = crud_core.update(Autos).where(Autos.c.brand == 'Mazda 6').values(brand='BMW X5')
+
+    # select_test1 = crud_core.select(Autos)
+    # print(conn.execute(select_test1).all())
 
     # # # +
     # delete_1 = crud_core.delete(Autos).where(Autos.c.brand == 'Audi Q5')
@@ -122,17 +117,12 @@ with engine.connect() as conn:
     # select_test2 = crud_core.select(Autos)
     # print(conn.execute(select_test2).all())
 
-
     # # -
     # delete_2 = crud_core.delete(Autos).where(Autos.c.brand == 'Audi Q5', Autos.c.rental_price == 7000)
     # conn.execute(delete_2)
 
     # select_test3 = crud_core.select(Autos)
     # print(conn.execute(select_test3).all())
-    
-    # +
-    # select_1 = crud_core.select(Autos.c.brand).group_by(Autos.c.brand)
-    # print(conn.execute(select_1).all())
-    
-    
+
     conn.commit()
+    
